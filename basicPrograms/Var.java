18421d10bac09088830a27025022375931a84bc2
Shruti Manoorkar <shrutimanoorkar1404@gmail.com>
2025-08-08 10:31:50 +0530

    Add files via upload
    Basic Programs Of java

---
+++ basicPrograms/Var.java
@@ -0,0 +1,35 @@
+package com.basicPrograms;
+
+public class Var {
+	
+	public static void main(String args[]) {
+		
+		// Primitive datatypes
+		
+		byte a = 45;                //1 byte
+		short b = 500;              // 2 byte   1 byte= 8 bits
+		int c = 456778666;          // 4 byte
+		long d = 345678879L;        // 8 byte
+		float e = 23.56f;           // 4 byte
+		double f = 23.45566564332245665435546545d;  //  8 byte
+		char g = 'g';               // 2 byte
+		boolean h = true;           // 1 bit    default value is False668
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		
+		
+		// Non Primitive datatypes
+		
+		String s= "shruti";
+				System.out.println(s);			
+		
+	}
+
+}
